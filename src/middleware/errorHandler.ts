54bc4a4cf3e83e7e73d87b61fe191a2ce7b7f738
ahmad5373 <ahmadraxa550@gmail.com>
2025-04-30 15:45:18 +0500

    console error

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -3,6 +3,7 @@ import { sendResponse } from "../utility/apiResponse";
 
 export const errorHandler = async (err:any, req:Request , res: Response, next: NextFunction): Promise<any> =>{
     console.log("in error handler....");
+    console.log("in error....", err);
     const errStatus = err?.code || 500;
     const errMsg = err?.message || 'Simething went wrong';
     return sendResponse(res, errStatus, errMsg);
